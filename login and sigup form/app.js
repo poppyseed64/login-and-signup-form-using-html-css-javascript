d6ba413224ebf93529f9ac89000cefd8190a5974
vignesh <64148081+poppyseed64@users.noreply.github.com>
2020-09-09 20:25:04 +0530

    first commit

---
+++ login and sigup form/app.js
@@ -0,0 +1,14 @@
+
+function redirect()
+{
+var url = "./signup.html";
+window.location.href=url;
+}
+function homeredirect(){
+    var url = "./index.html";
+ window.location.href=url;
+}
+function loginredirect(){
+    var url = "./login.html";
+ window.location.href=url;
+}
